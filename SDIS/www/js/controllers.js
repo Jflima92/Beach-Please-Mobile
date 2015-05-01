6810ee5cbaea009fb577bc997416170a50c7453c
Jorge Lima <ei10102@fe.up.pt>
2015-05-01 23:28:20 +0100

    Google map api attempt

--- SDIS/www/js/controllers.js
+++ SDIS/www/js/controllers.js
@@ -1,48 +1,105 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+      // Form data for the login modal
+      $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function() {
+        $scope.modal.hide();
+      };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+      // Open the login modal
+      $scope.login = function() {
+        $scope.modal.show();
+      };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('HomeCtrl', function($scope, $ionicLoading, $compile) {
+        function initialize() {
+          var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+          var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          var map = new google.maps.Map(document.getElementById("map"),
+              mapOptions);
+
+          //Marker + infowindow + angularjs compiled ng-click
+          var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+          var compiled = $compile(contentString)($scope);
+
+          var infowindow = new google.maps.InfoWindow({
+            content: compiled[0]
+          });
+
+          var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Uluru (Ayers Rock)'
+          });
+
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+          });
+
+          $scope.map = map;
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+
+        $scope.centerOnMe = function() {
+          if(!$scope.map) {
+            return;
+          }
+
+          $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+          });
+
+          navigator.geolocation.getCurrentPosition(function(pos) {
+            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            $scope.loading.hide();
+          }, function(error) {
+            alert('Unable to get location: ' + error.message);
+          });
+        };
+
+        $scope.clickTest = function() {
+          alert('Example of infowindow with ng-click')
+        };
+    })
+
+    .controller('PlaylistsCtrl', function($scope) {
+      $scope.playlists = [
+        { title: 'Reggae', id: 1 },
+        { title: 'Chill', id: 2 },
+        { title: 'Dubstep', id: 3 },
+        { title: 'Indie', id: 4 },
+        { title: 'Rap', id: 5 },
+        { title: 'Cowbell', id: 6 }
+      ];
+    })
+
+    .controller('PlaylistCtrl', function($scope, $stateParams) {
+    });
