0108df00d9f37546692a822bd30925b3e2e3a0a8
Jorge Lima <ei10102@fe.up.pt>
2015-05-12 19:44:03 +0100

    using module request instead of http now

--- server/routes/beaches.js
+++ server/routes/beaches.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var beach = require('../models/beach.js');
+var request = require('request');
 
 /* GET /todos listing. */
 router.get('/', function(req, res, next) {
@@ -12,75 +13,39 @@ router.get('/', function(req, res, next) {
         var coords= "";
 
         for(i=0;i<todos.length-1;i++){
-
             coords += todos[i]["lat"] + ","+todos[i]["lng"]+ "|" ;
-
         }
 
         coords += todos[todos.length-1]["lat"] + ","+todos[todos.length-1]["lng"] ;
-
-
 
         var url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=" + "41.1778751,-8.597915999999941"+ "&destinations="+ coords;
 
         var cenas ={
             "coords": coords,
             "url": url
-
         };
 
-        var http = require("http");
-
-        var request = http.get(url, function (response) {
-            // data is streamed in chunks from the server
-            // so we have to handle the "data" event
-            var buffer = "",
-                data,
-                route;
-
-            response.on("data", function (chunk) {
-                buffer += chunk;
-            });
-
-            response.on("end", function (err) {
-                // finished transferring data
-                // dump the raw data
-
-                data = JSON.parse(buffer);
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                data = JSON.parse(body);
 
                 var results = [];
 
-
                 console.log(data["rows"][0]["elements"]);
-
-                //rows = JSON.parse(data["rows"]);
-
-
-
-
 
                 for(var i=0; i<data["rows"][0]["elements"].length;i++){
 
-
                     console.log("fora");
                     console.log(data["rows"][0]["elements"][i]["distance"]["value"]);
-                    if (data["rows"][0]["elements"][i]["distance"]["value"]<20000){
 
+                    if (data["rows"][0]["elements"][i]["distance"]["value"]<10000){
                         console.log("entrou");
                         results.push(todos[i]);
-
                     }
-
                 }
-
                 res.json(results);
-
-                // extract the distance and time
-
-            });
-        });
-
-
+            }
+        })
         //res.json(cenas);
     });
 });
