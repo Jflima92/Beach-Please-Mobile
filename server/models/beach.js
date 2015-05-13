59a5200fb2937595f5c065dee0a0ca4902556557
zlima <jlima91@hotmail.com>
2015-05-13 01:04:11 +0100

    beach.js weatherCond.js models

--- server/models/beach.js
+++ server/models/beach.js
@@ -4,6 +4,9 @@ var BeachSchema = new mongoose.Schema({
     name: String,
     lat: Number,
     lng: Number,
-});
+    cond : {type: mongoose.Schema.ObjectId, ref: 'weatherCond'}
+},
+    {collection : "beaches"}
+);
 
 module.exports = mongoose.model('Beach', BeachSchema);
